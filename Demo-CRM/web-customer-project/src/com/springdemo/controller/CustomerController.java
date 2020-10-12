294121b8635e2dd68fa01c3ea56b35c1ce5ef7f3
kosta <dxkosta@gmail.com>
2020-10-12 16:39:34 +0300

    Add files via upload
    CRM demo 
    using spring, hibernate, and MySQL DB

---
+++ Demo-CRM/web-customer-project/src/com/springdemo/controller/CustomerController.java
@@ -0,0 +1,99 @@
+package com.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.springdemo.dao.CustomerDAO;
+import com.springdemo.entity.Customer;
+import com.springdemo.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+
+	@Autowired
+	CustomerService customerService;
+
+	@GetMapping("/list")
+	public String listCustomers(Model theModel) {
+
+		//get the customers from service from dao from db 
+		List<Customer> theCustomers = customerService.getCustomers();
+
+		//add the customers to the model
+		theModel.addAttribute("customers",theCustomers);
+
+		return "list-customers";
+	}
+
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+
+		//create model attribudte to bind form data 
+		Customer theCustomer = new Customer();
+
+		theModel.addAttribute("customer",theCustomer);
+
+
+		return "customer-form";
+	}
+
+
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+
+		//save the customer 
+		customerService.saveCustomer(theCustomer);
+
+		return "redirect:/customer/list";
+
+	}
+
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId")int id,Model theModel) {
+
+		//get the customer from the service 
+		Customer theCustomer = customerService.getCustomer(id);
+
+		//set as a model attribute to pre-populate the form 
+		theModel.addAttribute("customer",theCustomer);
+
+		//send over to our form
+
+		return "customer-form";
+	}
+
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId")int id) {
+		//delete the customer
+		customerService.deleteCustomer(id);
+
+		return "redirect:/customer/list";
+	}
+
+	@GetMapping("/search")
+
+    public String searchCustomers(@RequestParam("theSearchName") String theSearchName,
+			Model theModel) {
+
+		// search customers from the service
+		List<Customer> theCustomers = customerService.searchCustomers(theSearchName);
+
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
+
+		return "list-customers";        
+	}
+
+}
---
+++ Demo-CRM/web-customer-project/src/com/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,18 @@
+package com.springdemo.dao;
+
+import java.util.List;
+
+import com.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int id);
+
+	public void deleteCustomer(int id);
+
+	public List<Customer> searchCustomers(String theSearchName);
+}
